7978c9ffb24bce68f7a1014d5567e7aa5b072caa
Colin Reddig <crreddig@gmail.com>
2026-02-24 18:24:01 -0500

    Set s3 policy to use static website config

--- environments/prod/artifacts/.terragrunt-cache/x-GHZ-plb69BQp1kQuItLYy5OJ0/QukyuXn6NBQQy3DW_sHau0UUBZo/cloudfront.tf
+++ environments/prod/artifacts/.terragrunt-cache/x-GHZ-plb69BQp1kQuItLYy5OJ0/QukyuXn6NBQQy3DW_sHau0UUBZo/cloudfront.tf
@@ -11,11 +11,17 @@ resource "aws_cloudfront_distribution" "this" {
 
   origin {
     origin_id                = local.s3_origin_id
-    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+    domain_name              = aws_s3_bucket_website_configuration.this.website_endpoint
+    # origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+    custom_origin_config  {
+          origin_protocol_policy = "http-only"
+          http_port  = "80"
+          https_port = "443"
+          origin_ssl_protocols = ["TLSv1.2"]
+        }
   }
 
-  aliases = [var.domain_name]
+  aliases = [var.domain_name, "www.${var.domain_name}"]
 
   default_cache_behavior {
     
@@ -50,8 +56,53 @@ resource "aws_cloudfront_distribution" "this" {
 
   price_class = "PriceClass_200"
 
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
   
-  
+  # ordered_cache_behavior {
+  #   path_pattern     = "/posts/*"
+  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods   = ["GET", "HEAD"]
+  #   target_origin_id = local.s3_origin_id
+
+  #   forwarded_values {
+  #     query_string = false
+
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
+
+  #   min_ttl                = 0
+  #   default_ttl            = 3600
+  #   max_ttl                = 86400
+  #   compress               = true
+  #   viewer_protocol_policy = "redirect-to-https"
+  # }
+
+  #   ordered_cache_behavior {
+  #   path_pattern     = "/projects"
+  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods   = ["GET", "HEAD"]
+  #   target_origin_id = local.s3_origin_id
+
+  #   forwarded_values {
+  #     query_string = false
+
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
+
+  #   min_ttl                = 0
+  #   default_ttl            = 3600
+  #   max_ttl                = 86400
+  #   compress               = true
+  #   viewer_protocol_policy = "redirect-to-https"
+  # }
 }
 
 resource "aws_cloudfront_origin_access_control" "this" {
--- environments/prod/artifacts/.terragrunt-cache/x-GHZ-plb69BQp1kQuItLYy5OJ0/QukyuXn6NBQQy3DW_sHau0UUBZo/s3_policy.tf
+++ environments/prod/artifacts/.terragrunt-cache/x-GHZ-plb69BQp1kQuItLYy5OJ0/QukyuXn6NBQQy3DW_sHau0UUBZo/s3_policy.tf
@@ -16,32 +16,61 @@ resource "aws_s3_bucket_website_configuration" "this" {
 
 }
 
+# data "aws_iam_policy_document" "origin_bucket_policy" {
+#   statement {
+#     sid    = "AllowCloudFrontServicePrincipalReadWrite"
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["cloudfront.amazonaws.com"]
+#     }
+
+#     actions = [
+#       "s3:GetObject",
+#       "s3:PutObject",
+#     ]
+
+#     resources = [
+#       "${aws_s3_bucket.this.arn}/*",
+#     ]
+
+#     condition {
+#       test     = "StringEquals"
+#       variable = "AWS:SourceArn"
+#       values   = [aws_cloudfront_distribution.this.arn]
+#     }
+#   }
+# }
+
 data "aws_iam_policy_document" "origin_bucket_policy" {
   statement {
     sid    = "AllowCloudFrontServicePrincipalReadWrite"
     effect = "Allow"
 
     principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
+      type        = "*"
+      identifiers = ["*"]
     }
-
     actions = [
       "s3:GetObject",
-      "s3:PutObject",
+      # "s3:PutObject",
     ]
 
     resources = [
       "${aws_s3_bucket.this.arn}/*",
     ]
 
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.this.arn]
-    }
+
   }
 }
+
+# resource "aws_s3_bucket_public_access_block" "example" {
+#   bucket = aws_s3_bucket.example.id
+
+#   block_public_acls   = false
+#   block_public_policy = false
+# }
 
 resource "aws_s3_bucket_policy" "b" {
   bucket = aws_s3_bucket.this.bucket
