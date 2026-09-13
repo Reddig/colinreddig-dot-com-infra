ccb64dc6660eefe406914d9aaaa3798c75fb901f
Colin Reddig <crreddig@gmail.com>
2026-09-13 11:44:40 -0400

    lock down s3 bucket

--- modules/artifacts/s3_policy.tf
+++ modules/artifacts/s3_policy.tf
@@ -16,54 +16,54 @@ resource "aws_s3_bucket_website_configuration" "this" {
 
 }
 
-# data "aws_iam_policy_document" "origin_bucket_policy" {
-#   statement {
-#     sid    = "AllowCloudFrontServicePrincipalReadWrite"
-#     effect = "Allow"
-
-#     principals {
-#       type        = "Service"
-#       identifiers = ["cloudfront.amazonaws.com"]
-#     }
-
-#     actions = [
-#       "s3:GetObject",
-#       "s3:PutObject",
-#     ]
-
-#     resources = [
-#       "${aws_s3_bucket.this.arn}/*",
-#     ]
-
-#     condition {
-#       test     = "StringEquals"
-#       variable = "AWS:SourceArn"
-#       values   = [aws_cloudfront_distribution.this.arn]
-#     }
-#   }
-# }
-
 data "aws_iam_policy_document" "origin_bucket_policy" {
   statement {
     sid    = "AllowCloudFrontServicePrincipalReadWrite"
     effect = "Allow"
 
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
+
     actions = [
       "s3:GetObject",
-      # "s3:PutObject",
+      "s3:PutObject",
     ]
 
     resources = [
       "${aws_s3_bucket.this.arn}/*",
     ]
 
-
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
   }
 }
+
+# data "aws_iam_policy_document" "origin_bucket_policy" {
+#   statement {
+#     sid    = "AllowCloudFrontServicePrincipalReadWrite"
+#     effect = "Allow"
+
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
+#     actions = [
+#       "s3:GetObject",
+#       # "s3:PutObject",
+#     ]
+
+#     resources = [
+#       "${aws_s3_bucket.this.arn}/*",
+#     ]
+
+
+#   }
+# }
 
 # resource "aws_s3_bucket_public_access_block" "example" {
 #   bucket = aws_s3_bucket.example.id
